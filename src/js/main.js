87923bc2e0282b189d79ef43472c890b199de44e
jaris <jarisma@foxmail.com>
2020-01-03 12:24:23 +0800

    实现响应式广告

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: jarisMA
+ * @Date: 2019-12-19 15:53:04
+ * @LastEditTime : 2020-01-02 15:55:14
+ * @LastEditors  : jarisMA
+ */
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true
+  });
+})
